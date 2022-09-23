e520e3724a7ba9a2f819ace0fd768c150af715a1
Edwin Harmuth <102912402+MarioNaise@users.noreply.github.com>
2022-09-23 15:37:36 +0000

    steps 1-12

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const text = $("#text");
 const author = $("#author");
 const newQuote = $("#new-quote");
 const tweetQuote = $("#tweet-quote");
-const quoteUrl = "http://quotable.io/random";
+const quoteUrl = "https://quotable.io/random";
 const link =
     "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=";
 let query = "";
@@ -11,8 +11,9 @@ async function updateQuote(url) {
     const response = await fetch(url);
     let data = await response.json();
     text.html(data.content);
-    author.html(data.author);
+    author.html("- "+data.author);
     query = encodeURI(data.content) + encodeURI(data.author);
+  tweetQuote[0].href = link + query;
 }
 
 updateQuote(quoteUrl);
