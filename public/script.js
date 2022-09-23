d6dba8970c77d89c3a1b8e2aa6538b3ce20812c6
Edwin Harmuth <edwin.harmuth@web.de>
2022-09-23 17:11:51 +0200

    steps 1-12

---
+++ public/script.js
@@ -0,0 +1,22 @@
+const text = $("#text");
+const author = $("#author");
+const newQuote = $("#new-quote");
+const tweetQuote = $("#tweet-quote");
+const quoteUrl = "http://quotable.io/random";
+const link =
+    "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=";
+let query = "";
+
+async function updateQuote(url) {
+    const response = await fetch(url);
+    let data = await response.json();
+    text.html(data.content);
+    author.html(data.author);
+    query = encodeURI(data.content) + encodeURI(data.author);
+}
+
+updateQuote(quoteUrl);
+
+newQuote.on("click", () => {
+    updateQuote(quoteUrl);
+});
--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/",(req, res)=>{
-    res.sendFile(__dirname+"/public/index.html")
-})
+app.use(express.static(__dirname+"/public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(PORT, () => {
     console.log("Server is listening on PORT: " + PORT);
 });
-
-
-const quoteUrl = "https://zenquotes.io/api/random";
